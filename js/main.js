32d0cd342e6aea5c8add91c38573b4dc4793a88f
Vlad Nicula <vladnicula@gmail.com>
2014-03-15 16:21:07 +0200

    Added jquery, backbone, udnerscore and requirejs.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-(function($, undefined){ 
+require([],function () {
 
-'use strict'; 
+  'use strict'; 
 
-$(function () {
+  $(function () {
 
-  var secondChart = new CanvasJS.Chart("invoicesPerMonthChartContainer", {
-      title:{
-          text: "Invoices per month"              
-      },
-      data: [              
-        {
-            type: "column",
-            dataPoints: [
-              { label: "jan", y: 10 },
-              { label: "feb", y: 15 },
-              { label: "mar", y: 25 },
-              { label: "apr", y: 30 },
-              { label: "may", y: 28 }
-            ]
-        }
-      ]
+    var secondChart = new CanvasJS.Chart("invoicesPerMonthChartContainer", {
+        title:{
+            text: "Invoices per month"              
+        },
+        data: [              
+          {
+              type: "column",
+              dataPoints: [
+                { label: "jan", y: 10 },
+                { label: "feb", y: 15 },
+                { label: "mar", y: 25 },
+                { label: "apr", y: 30 },
+                { label: "may", y: 28 }
+              ]
+          }
+        ]
+    });
+
+    secondChart.render();
+
   });
 
-  secondChart.render();
-
 });
-
-}(jQuery));

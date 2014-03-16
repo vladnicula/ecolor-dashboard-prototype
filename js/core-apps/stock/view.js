70e16c24ac0b5407561eaf429ecf7e63e0e52251
Vlad Nicula <vladnicula@gmail.com>
2014-03-16 13:55:59 +0200

    Chart on stock page.

--- js/core-apps/stock/view.js
+++ js/core-apps/stock/view.js
@@ -16,6 +16,48 @@ define("core-apps/stock/view", [
         view.$el.html(tpl({}));
 
         return this;
+      },
+
+      show : function () {
+        var view = this,
+          chart = new CanvasJS.Chart("stock-chart", {
+            title:{
+              text: "Stock Value"
+            },   
+            data: [{        
+              type: "stackedArea", //or stackedColumn
+              dataPoints: [
+              { x: 10, y: 171 },
+              { x: 20, y: 155},
+              { x: 30, y: 150 },
+              { x: 40, y: 165 },
+              { x: 50, y: 195 },
+              { x: 60, y: 168 },
+              { x: 70, y: 128 },
+              { x: 80, y: 134 },
+              { x: 90, y: 114}
+              ]
+            },{        
+              type: "stackedArea", //or stackedColumn
+              dataPoints: [
+              { x: 10, y: 101 },
+              { x: 20, y: 105},
+              { x: 30, y: 100 },
+              { x: 40, y: 105 },
+              { x: 50, y: 105 },
+              { x: 60, y: 108 },
+              { x: 70, y: 108 },
+              { x: 80, y: 104 },
+              { x: 90, y: 104}
+              ]
+            }
+            ]
+          });
+
+        PageView.prototype.show.apply(this, arguments);
+
+        chart.render();
+        return this;
       }
   });
 });
--- js/core-apps/stock/view.tpl
+++ js/core-apps/stock/view.tpl
@@ -1,1 +1,34 @@
-<h2>Stock</h2>
+<div class="container">
+
+  <div class="home-intro-template">
+	<div class="info-section stock-info-section">
+		<div class="info-sub-section stock-chart-section">
+			<div class="chart-container" id='stock-chart'></div>
+		</div>
+		<div class="info-sub-section stock-current-info">
+			<div class="info-wrapper">
+				<h2 class='info-head'>ECOLOR STOCK PRICE</h2>
+				<p class='info-sub-head stock-curency'>US Dollars</p>
+			</div>
+			<p class='info-important-number stock-current-value'>307.90</p>
+		</div>
+		<div class="info-sub-section stock-statistics-info">
+			<h2 class='info-head'>STOCK MARKET TRENDS</h2>
+			<p class='info-sub-head'>Thruout the year</p>
+			<div class="status-box">
+				<h3 class='info-head'>HIGHEST STOCK PRICE</h3>
+				<p class='info-number green'>505.3 <span class='iconmoon-icon icon-arrow-up-right'></span></p>
+			</div>
+			<div class="status-box stock-lowest">
+				<h3 class='info-head'>LOWEST STOCK PRICE</h3>
+				<p class='info-number red'>205.3<span class='iconmoon-icon icon-arrow-down-right'></span></p>
+			</div>
+			<div class="status-box stock-yoy-change">
+				<h3 class='info-head'>YOY CHANGE </h3>
+				<p class='info-number'>-15.3%</p>
+			</div>
+		</div>
+	</div>
+	<!-- end info-section -->
+    </div>
+</div>	
